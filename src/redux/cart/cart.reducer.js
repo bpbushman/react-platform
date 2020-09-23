3aa511635574a1bbfcfe38dfb88442892f468249
Brian <brian@Brians-MacBook-Air.local>
2020-09-23 16:03:25 -0600

    added remove item from cart functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -8,6 +8,13 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
+            }
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
