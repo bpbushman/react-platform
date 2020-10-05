aaa6acd2ec0869253780b019d41273d839638dfa
Brian <brian@Brians-MacBook-Air.local>
2020-10-05 14:25:49 -0600

    .

--- src/pages/checkoutpage/checkout.component.jsx
+++ src/pages/checkoutpage/checkout.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import{ connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
