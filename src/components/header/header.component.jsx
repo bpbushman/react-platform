61338396c8859c02297980744c2545e7f26b2231
Brian <brian@Brians-MacBook-Air.local>
2020-10-07 12:50:45 -0600

    replaced sass with styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
@@ -9,29 +8,29 @@ import { selectCurrentUser } from '../../redux/user/user.selector';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropDown from '../cart/cart.component';
 
-import './header.styles.scss';
+import {HeaderContainer,OptionDiv,OptionLink,OptionsContainer,LogoContainer} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-    <div className='header'>
-        <Link to='/'>
+    <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 CONTACT
-            </Link>
+            </OptionLink>
             {currentUser
-                ? (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
-                : (<Link className='option' to='/signin'> SIGN IN</Link>)}
+                ? (<OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>)
+                : (<OptionLink to='/signin'> SIGN IN</OptionLink>)}
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {hidden
             ? null
             : <CartDropDown />}
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps =  createStructuredSelector({
