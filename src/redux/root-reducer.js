92e2138fec5d5b9940ce7fdd44d62bda4f246177
Brian <brian@Brians-MacBook-Air.local>
2020-10-07 11:30:17 -0600

    .

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -7,7 +7,7 @@ import cartReducer from './cart/cart.reducer';
 import directoryReducer from './directory/directory.reducer';
 import shopReducer from './shop/shop.reducer';
 
-const persistCofig = {
+const persistConfig = {
     key: 'root',
     storage,
     whitelist: ['cart']
@@ -20,4 +20,4 @@ const rootReducer = combineReducers({
     shop: shopReducer
 });
 
-export default persistReducer(persistCofig, rootReducer);
+export default persistReducer(persistConfig, rootReducer);
